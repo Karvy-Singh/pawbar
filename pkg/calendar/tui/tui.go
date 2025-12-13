1f0738ac0ec20809d1ec1a86c09d494462a24877
Karvy-Singh <karvysingh14@gmail.com>
2025-12-13 20:52:47 +0530

    init: per month calender

---
+++ pkg/calendar/tui/tui.go
@@ -0,0 +1,55 @@
+package tui
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"time"
+
+	//"git.sr.ht/~rockorager/vaxis"
+	"github.com/nekorg/katnip"
+)
+
+func Panel(k *katnip.Kitty, rw io.ReadWriter) int {
+	PrintCurrentMonthCal()
+	select {}
+}
+
+func PrintCurrentMonthCal() {
+	now := time.Now()
+	PrintMonthCal(now.Year(), now.Month())
+}
+
+func PrintMonthCal(year int, month time.Month) {
+	const width = 20
+
+	title := fmt.Sprintf("%s %d", month, year)
+	fmt.Println(center(title, width))
+	fmt.Println("Su Mo Tu We Th Fr Sa")
+
+	loc := time.Now().Location()
+	first := time.Date(year, month, 1, 0, 0, 0, 0, loc)
+	offset := int(first.Weekday())
+	daysInMonth := time.Date(year, month+1, 0, 0, 0, 0, 0, loc).Day()
+	for i := 0; i < offset; i++ {
+		fmt.Print("   ") // 2 digits + space
+	}
+	for day := 1; day <= daysInMonth; day++ {
+		fmt.Printf("%2d ", day)
+		wd := (offset + (day - 1)) % 7
+		if wd == 6 {
+			fmt.Println()
+		}
+	}
+	if (offset+daysInMonth-1)%7 != 6 {
+		fmt.Println()
+	}
+}
+
+func center(s string, width int) string {
+	if len(s) >= width {
+		return s
+	}
+	left := (width - len(s)) / 2
+	return strings.Repeat(" ", left) + s
+}
